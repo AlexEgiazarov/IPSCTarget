Added target image show and correct shot placement calculation

ipsctarget.py
@@ -11,19 +11,36 @@ class Targetipsc():
         self.t_points = target_points
 
         #create rectangle based on chosen points
-        self.target_rect = cv2.boundingRect(target_points)
+        self.target_rect = self.get_t_rectangle(target_points)
+
+
+        print("Target rectangle")
+        print(self.target_rect)
 
         #other variables
-        self.t_trans_points = [[205,30], [385,30], [570,260], [570, 490], [390,722], [250,722], [22,490], [22,260]]
+        #self.t_trans_points = [[205,30], [385,30], [570,260], [570, 490], [390,722], [250,722], [22,490], [22,260]]
+        self.t_trans_points = [[0,0], [565,0], [565,713], [0, 713]]
         self.t_mask = []
         self.transform_matrix = self.get_transform()
 
         #Loading image
         self.t_image_init = cv2.imread("ipsctarget1.jpg")
-        self.t_image = self.t_image_init.copy
+        self.t_image = cv2.imread("ipsctarget1.jpg")
 
         #setting target id
         self.target_id = t_id
+
+    '''
+    Make target rectangle
+    '''
+    def get_t_rectangle(self, t_points):
+        (x,y,w,h) = cv2.boundingRect(t_points)
+        t1 = [x,y]
+        t2 = [x+w,y]
+        t3 = [x+w,y+h]
+        t4 = [x,y+h]
+        rect = [t1,t2,t3,t4]
+        return rect
 
     #return id
     def get_id(self):
@@ -52,15 +69,18 @@ class Targetipsc():
     def get_transform(self):
 
         #convert points
-        pts1 = np.float32(self.t_points)
+        #pts1 = np.float32(self.t_points)
+        pts1 = np.float32(self.target_rect)
         print(pts1)
         pts2 = np.float32(self.t_trans_points)
         print(pts2)
 
         #geting transform
         #self.transform_matrix = cv2.getPerspectiveTransform(pts1,pts2)
+        M = cv2.getPerspectiveTransform(pts1, pts2)
+        #M = cv2.getAffineTransform(pts1, pts2)
 
-        return cv2.getPerspectiveTransform(pts1, pts2)
+        return M
 
     '''
     Make target mask
@@ -84,10 +104,17 @@ class Targetipsc():
     def update_target(self, shot_coordinates):
         print("Updating target")
 
-        #dst = cv2.warpPerspective(shot_coordinates,self.transform_matrix,(300,300))
-        relative_shot = cv2.transform(shot_coordinates, self.transform_matrix)
+        #shot_coordinates = np.float32(shot_coordinates)
+        shot_coordinates = (shot_coordinates[0], shot_coordinates[1], 1)
 
-        cv2.circle(self.t_image, relative_shot, 3, (0,0,255), -1)
+        #calculate shot placement on target
+        relative_shot = np.array(self.transform_matrix) @ np.array(shot_coordinates)
+
+        #reassemble shot placement
+        relative_shot = (int(relative_shot[0]), int(relative_shot[1]))
+        
+        #cv2.circle(self.t_image, relative_shot, 3, (0,0,255), -1)
+        return relative_shot
 
 
     '''

range.py
@@ -18,6 +18,8 @@ class Range():
         self.red_threshold = 230
         self.target_points = []
         self.targets = []
+        self.target_image_set = []
+        self.target_show = []
         self.wait = False
 
     '''
@@ -88,9 +90,17 @@ class Range():
             #convert crop points
             self.target_points = np.asarray(np.divide(self.target_points,[self.re_scale_factor_x,self.re_scale_factor_y]),int)
 
+            #create target 
             target = Targetipsc(self.target_points, len(self.targets))
 
+            #append target to the list
             self.targets.append(target)
+
+            #TODO #NEED TO CHANGE THIS 
+            self.target_image_set.append(cv2.imread("ipsctarget1.jpg"))
+
+            #reset target points
+            self.target_points = []
 
             #reset all cv2 windows
             cv2.destroyAllWindows()
@@ -113,8 +123,11 @@ class Range():
                     if len(c)>=4:
                         # compute the center of the contour
                         M = cv2.moments(c)
-                        cX = M["m10"] / M["m00"]
-                        cY = M["m01"] / M["m00"]
+                        try:
+                            cX = M["m10"] / M["m00"]
+                            cY = M["m01"] / M["m00"]
+                        except ZeroDivisionError:
+                            return None
 
                         return (cX, cY)
             else:
@@ -159,6 +172,7 @@ class Range():
                 #destroy calibration window
                 cv2.destroyWindow("Calibration")
 
+
     '''
     Main run function
     '''
@@ -177,8 +191,14 @@ class Range():
             #read frame
             ret, frame = cap.read()
 
-            #show frame
-            cv2.imshow('frame', frame)
+
+
+            if len(self.target_image_set)>0:
+                #show frame
+                for idx, t_i in enumerate(self.target_image_set):
+                    cv2.imshow("Target - {}".format(idx), t_i)
+            else:
+                cv2.imshow('frame', frame)
 
             #if no targets made
             if len(self.targets) == 0:
@@ -212,11 +232,14 @@ class Range():
                         hit_status = False
 
                         #for each target in list
-                        for t in self.targets:
+                        for idx, t in enumerate(self.targets):
                             
                             #check if shot is inside the target
                             if t.inside_target(shot_status):
                                 print("Target [{}] HIT".format(t.get_id()))
+                                #self.target_image_set[idx] = t.get_target_image()
+                                relative_shot = t.update_target(shot_status)
+                                cv2.circle(self.target_image_set[idx], relative_shot, 3, (0,0,255), -1)
                                 hit_status = True
 
                         #check if hits were registered